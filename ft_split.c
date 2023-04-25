9c1f1c75367f61bf11280b117c1d34b311a6c732
mvisca <martin.visca@gmail.com>
2023-04-25 07:54:37 +0200

    ft_split.c push2

--- ft_split.c
+++ ft_split.c
@@ -38,13 +38,33 @@ char	**ft_split(const char *s, char c)
 {
 	int		arr_len;
 	int		max;
+	int		i;
+	int		j;
 	char	**res;
+	char	*str;
 
 	arr_len = c_count(s, c);
 	max = s_max(s, c);
 
-	res = (char **) calloc (arr_len, max);
+	res = (char **) calloc (arr_len + 1, max + 1);
 	if (res == NULL)
 		return (NULL);
 	
+	i = 0;
+	j = 0;
+	while ((s + i) < strchr((s + i), c))
+	{
+		if (*(s + i) == c)
+		{
+			*(*(res + j)) = '\0';
+			i++;
+			j = 0;
+		}
+		else 
+			*(*(res + j)) = *(s + i);
+		i++;
+		j++;
+	}
+	*(res + arr_len) = NULL;
+	return (res);
 }
