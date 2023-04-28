e8787854f00cc4855286bb145002147d1b805e50
Martin Visca Garat <mvisca-g@cbr10s2.42barcelona.com>
2023-04-28 20:01:29 +0200

    update memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,17 +1,42 @@
-void	*ft_memchr(const void *s, int c, ft_size_t n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvisca-g <mvisca-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 19:40:02 by mvisca-g          #+#    #+#             */
+/*   Updated: 2023/04/28 19:47:52 by mvisca-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, t_size_t n)
 {
 	unsigned char	*ptr;
 	unsigned char	uc;
-	ft_size_t		i;
+	t_size_t		i;
 
-	ptr = (unsigned char)s;
+	ptr = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
 		uc = (unsigned char)c;
 		if (ptr[i] == uc)
-			return (s + i);
+			return (ptr + i);
 		i++;
 	}
 	return (NULL);
 }
+
+/*
+int	main(void)
+{
+	char str[22] = "buscando una x aqui";
+	char c = 'f';
+
+	printf ("%s", ft_memchr(str, c, 22));
+	return (0);
+}
+*/
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,15 +1,41 @@
-int	ft_memcmp(const void *s1, const void *s2, ft_size_t n)
-{
-	unsigned char	us1;
-	unsigned char	us2;
-	ft_size_t		i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvisca-g <mvisca-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 19:48:34 by mvisca-g          #+#    #+#             */
+/*   Updated: 2023/04/28 20:00:34 by mvisca-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	us1 = (unsigned char)s1;
-	us2 = (unsigned char)s2;
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, t_size_t n)
+{
+	unsigned char	*us1;
+	unsigned char	*us2;
+	t_size_t		i;
+
+	us1 = (unsigned char *) s1;
+	us2 = (unsigned char *) s2;
 	i = 0;
 	while (i < n && us1[i] == us2[i])
 	{
 		i++;
 	}
-	return ((int)us1[i] - (int)us2[i]);
+	return (us1[i] - us2[i]);
 }
+
+/*
+int	main(void)
+{
+	char s[22] = "holalola";
+	char d[22] = "hola\200hola";
+
+	printf ("%d\n", ft_memcmp(s, d, 22));
+	printf ("%d\n", memcmp(s, d, 22));
+	return (0);
+}
+*/
