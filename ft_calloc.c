a7a938efa923f4e610b02cbb4eceae4a95546979
Martin Visca Garat <mvisca-g@car12s2.42barcelona.com>
2023-05-06 20:55:25 +0200

    fixes calloc, striteri

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mvisca-g <mvisca-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:49:31 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/05/04 13:52:08 by mvisca-g         ###   ########.fr       */
+/*   Updated: 2023/05/06 20:54:35 by mvisca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	ptr = NULL;
-	if (size == 0)
-		return (NULL);
-	if (nmemb == 0)
-		return (NULL);
-	if (nmemb * size > MAX_INT)
-		return (NULL);
 	ptr = (void *) malloc (nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
