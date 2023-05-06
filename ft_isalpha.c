226ce54f6dfa10d332096deb84ecb9bfa0eddd57
Martin Visca Garat <mvisca-g@car12s2.42barcelona.com>
2023-05-06 21:06:16 +0200

    isalpha fix

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,33 +6,27 @@
 /*   By: mvisca-g <mvisca-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 17:23:25 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/05/02 14:43:38 by mvisca-g         ###   ########.fr       */
+/*   Updated: 2023/05/06 21:05:22 by mvisca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static int	isupper(int c)
 {
-	if ((unsigned char) c >= 'A' && (unsigned char) c <= 'Z')
-	{
+	if (c >= 65 && c <= 90)
 		return (1);
-	}
 	return (0);
 }
 
 static int	islower(int c)
 {
-	if ((unsigned char) c >= 'a' && (unsigned char) c <= 'z')
-	{
+	if (c >= 97  && c <= 122)
 		return (1);
-	}
 	return (0);
 }
 
 int	ft_isalpha(int c)
 {
 	if (isupper(c) || islower(c))
-	{
 		return (1);
-	}
 	return (0);
 }
