75a77be49dcdba8fbb0085f76a274aa4faf4062c
Martin Visca Garat <mvisca-g@car7s4.42barcelona.com>
2023-05-09 20:53:05 +0200

    strict fails

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvisca-g <mvisca-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 20:46:02 by mvisca-g          #+#    #+#             */
+/*   Updated: 2023/05/09 20:52:41 by mvisca-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
@@ -8,11 +20,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst)
 		return (NULL);
 	new_list = ft_lstnew(f(lst->content));
+	if (!new_list)
+	{
+		del(new_list);
+		return (NULL);
+	}
 	curr_new = new_list;
 	while (lst->next)
 	{
 		lst = lst->next;
 		curr_new->next = ft_lstnew(f(lst->content));
+		if (!curr_new->next)
+		{
+			ft_lstclear(&curr_new, del);
+			return (NULL);
+		}
 		if (!curr_new->next)
 		{
 			del(curr_new->next);
