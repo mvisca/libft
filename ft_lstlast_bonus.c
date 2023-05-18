7500a60a3322d7c57e47ef8b3ce6017e84bfcdd8
mvisca <martin.visca@gmail.com>
2023-05-18 22:09:24 +0200

    ft_lstiter.c ft_lstlast.c ok

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -14,14 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*tmp;
-
-	if (lst == NULL)
-		return (NULL);
-	tmp = lst;
-	while (tmp->next)
-		tmp = tmp->next;
-	return (tmp);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
 
 /*
