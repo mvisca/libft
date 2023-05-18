a0d34bf0c209871a42f5037ba7c7588333c41db1
mvisca <martin.visca@gmail.com>
2023-05-18 21:49:33 +0200

    ft_isalpha ft_isascii ft_isdigit ft_isprint ok

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -10,23 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (1);
-	return (0);
-}
-
-static int	islower(int c)
-{
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
-}
-
 int	ft_isalpha(int c)
 {
-	if (isupper(c) || islower(c))
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
--- ft_isprint.c
+++ ft_isprint.c
@@ -13,8 +13,6 @@
 int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
-	{
 		return (1);
-	}
 	return (0);
 }
